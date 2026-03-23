7f31b5da03ed107e2bc81d6be70c4479401c0ea8
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2026-03-23 08:07:50 +0700

    feat: generate Post crud

---
+++ src/Form/PostType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\Tag;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('summary')
+            ->add('content')
+            ->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('title_id')
+            ->add('summary_id')
+            ->add('content_id')
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
